052cad9ac276b35584e7cf1e200b03b680d26576
Johnathan Middleton <johnathanmiddleton1@gmail.com>
2022-03-28 19:07:04 -0400

    useState > classes

--- client/src/App.js
+++ client/src/App.js
@@ -1,64 +1,61 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
 
-class App extends Component {
-  // Initialize state
-  state = { passwords: [] }
+function App() {
 
-  // Fetch passwords after first mount
-  componentDidMount() {
-    this.getPasswords();
-  }
+    const [passwords, setPasswords] = useState([]);
 
-  getPasswords = () => {
-    // Get the passwords and store them in state
-    fetch('/api/passwords')
-      .then(res => res.json())
-      .then(passwords => this.setState({ passwords }));
-  }
+    useEffect(() => {
+        getPasswords;
+    });
 
-  render() {
-    const { passwords } = this.state;
+    const getPasswords = () => {
+        // Get the passwords and store them in state
+        fetch('/api/passwords')
+            .then(res => res.json())
+            .then(passwords => setPasswords(passwords));
+    };
 
     return (
-      <div className="App">
-        {/* Render the passwords if we have them */}
-        {passwords.length ? (
-          <div>
-            <h1>5 Passwords.</h1>
-            <ul className="passwords">
-              {/*
+
+        <div className="App">
+            {/* Render the passwords if we have them */}
+            {passwords.length ? (
+                <div>
+                    <h1>5 Passwords.</h1>
+                    <ul className="passwords">
+                        {/*
                 Generally it's bad to use "index" as a key.
                 It's ok for this example because there will always
                 be the same number of passwords, and they never
                 change positions in the array.
               */}
-              {passwords.map((password, index) =>
-                <li key={index}>
-                  {password}
-                </li>
-              )}
-            </ul>
-            <button
-              className="more"
-              onClick={this.getPasswords}>
-              Get More
-            </button>
-          </div>
-        ) : (
-          // Render a helpful message otherwise
-          <div>
-            <h1>No passwords :(</h1>
-            <button
-              className="more"
-              onClick={this.getPasswords}>
-              Try Again?
-            </button>
-          </div>
-        )}
-      </div>
-    );
-  }
-}
+                        {passwords.map((password, index) =>
+                            <li key={index}>
+                                {password}
+                            </li>
+                        )}
+                    </ul>
+                    <button
+                        className="more"
+                        onClick={getPasswords}>
+                        Get More
+                    </button>
+                </div>
+            ) : (
+                // Render a helpful message otherwise
+                <div>
+                    <h1>No passwords :(</h1>
+                    <button
+                        className="more"
+                        onClick={getPasswords}>
+                        Try Again?
+                    </button>
+                </div>
+            )}
+        </div>
+    )
+
+};
 
 export default App;
