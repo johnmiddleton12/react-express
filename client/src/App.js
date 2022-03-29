e172cb2436bcece4b3152e5da853c528208e9637
Johnathan Middleton <johnathanmiddleton1@gmail.com>
2022-03-28 21:33:56 -0400

    test

--- client/src/App.js
+++ client/src/App.js
@@ -1,59 +1,34 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
+import { MantineProvider, ColorSchemeProvider, ColorScheme } from '@mantine/core';
+import { useLocalStorage } from '@mantine/hooks';
 import './App.css';
+import ToggleDark from './components/ToggleDark';
+import Passwords from './components/Passwords';
 
 export default function App() {
 
-    const [passwords, setPasswords] = useState([]);
+    const [colorScheme, setColorScheme] = useLocalStorage < ColorScheme > ({
+        key: 'mantine-color-scheme',
+        defaultValue: 'light',
+    });
 
-    useEffect(() => {
-        getPasswords();
-    }, []);
+    const toggleColorScheme = (ColorScheme) =>
+        setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'));
 
-    const getPasswords = () => {
-        // Get the passwords and store them in state
-        fetch('/api/passwords')
-            .then(res => res.json())
-            .then(passwords => setPasswords(passwords));
-    };
+
 
     return (
 
-        <div className="App">
-            {/* Render the passwords if we have them */}
-            {passwords.length ? (
-                <div>
-                    <h1>5 Passwords.</h1>
-                    <ul className="passwords">
-                        {/*
-                Generally it's bad to use "index" as a key.
-                It's ok for this example because there will always
-                be the same number of passwords, and they never
-                change positions in the array.
-              */}
-                        {passwords.map((password, index) =>
-                            <li key={index}>
-                                {password}
-                            </li>
-                        )}
-                    </ul>
-                    <button
-                        className="more"
-                        onClick={getPasswords}>
-                        Get More Passwords
-                    </button>
-                </div>
-            ) : (
-                // Render a helpful message otherwise
-                <div>
-                    <h1>No passwords :(</h1>
-                    <button
-                        className="more"
-                        onClick={getPasswords}>
-                        Try Again?
-                    </button>
-                </div>
-            )}
-        </div>
+        <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+            <MantineProvider theme={{ colorScheme }}>
+
+            <Passwords />
+
+            <ToggleDark toggleDark={toggleColorScheme}/>
+
+            </MantineProvider>
+
+        </ColorSchemeProvider>
     );
 
 };
